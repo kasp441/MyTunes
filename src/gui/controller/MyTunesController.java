a22d53daf3dffa272c5f2bc3ac90ae7991063be5
Tadiaki <n0m1z@hotmail.com>
2021-12-01 13:10:31 +0100

    Started MyTunesController with methods - still needs to be filled.

--- src/gui/controller/MyTunesController.java
+++ src/gui/controller/MyTunesController.java
@@ -1,4 +1,87 @@
 package gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+
 public class MyTunesController {
+
+    public void DeletePlaylistButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void EditPlaylistButton(ActionEvent actionEvent)
+    {
+
+    }
+    
+    public void NewPlaylistButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void DownButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void UpButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void FilterButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void AddSongToPlaylistButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void DeleteSongFromPlaylistButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void BackButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void PlayPauseButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void SkipButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void CloseApplicationButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void DeleteSongButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void EditSongButton(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void NewSongButton(ActionEvent actionEvent)
+    {
+
+    }
+
+
+
+
 }
