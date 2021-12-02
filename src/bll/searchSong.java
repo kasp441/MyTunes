3ef5b875065f3aaec4d84cfe08497315215e18c6
kasp441 <91524039+kasp441@users.noreply.github.com>
2021-12-02 13:12:32 +0100

    SearchSong
    created searchmethod in SearchSong

--- src/bll/searchSong.java
+++ src/bll/searchSong.java
@@ -1,7 +1,20 @@
 package bll;
 
+import be.Song;
 import javafx.collections.ObservableList;
 
-public class searchSong {
+public class SearchSong {
 
+    public ObservableList<Song> searchSong(ObservableList<Song> songList, String keyChar)
+    {
+        ObservableList<Song> resultSet = null;
+        for (Song song : songList)
+        {
+            if (keyChar.toLowerCase().contains(song.getTitle().toLowerCase()))
+            {
+                resultSet.add(song);
+            }
+        }
+        return resultSet;
+    }
 }
