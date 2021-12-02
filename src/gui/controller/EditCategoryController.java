a5d370d16291c09806f7fe5ce1057b1c8b1f4b41
Tadiaki <n0m1z@hotmail.com>
2021-12-02 12:52:59 +0100

    Category fxml and controller

---
+++ src/gui/controller/EditCategoryController.java
@@ -0,0 +1,11 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+
+public class EditCategoryController {
+    public void CancelCategoryButton(ActionEvent actionEvent) {
+    }
+
+    public void SaveCategoryButton(ActionEvent actionEvent) {
+    }
+}
