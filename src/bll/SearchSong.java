f704bfcd62c02b72b81e319e4d2477f0babb6848
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2021-12-03 10:53:26 +0100

    ny search song class

--- src/bll/SearchSong.java
+++ src/bll/SearchSong.java
@@ -4,9 +4,7 @@ import be.Song;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-
 public class SearchSong {
-
     public ObservableList<Song> searchSong(ObservableList<Song> songList, String keyChar)
     {
         ObservableList<Song> result = FXCollections.observableArrayList();
