a2ae22c08774f50bfa7513ce0e440bdf8e4f9b3a
MathiasKrarup <math249e@easv365.dk>
2021-12-03 10:41:55 +0100

    Tilføjelse af metoder i PlaylistDAO og PlaylistManager

--- src/dal/PlaylistDAO.java
+++ src/dal/PlaylistDAO.java
@@ -1,6 +1,7 @@
 package dal;
 
 import be.Playlist;
+import be.Song;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 
 import java.io.IOException;
@@ -37,6 +38,53 @@ public class PlaylistDAO {
         return allPlaylists;
     }
 
+    public Playlist createPlaylist(String playlistName, int totallenght, int totalsongs) throws SQLException {
+        int newID = -1;
+        String sql = "INSERT INTO Playlist(PlaylistName, Totallenght, TotalSongs) VALUES (?, ?, ?)";
+        try(Connection connection = databaseConnector.getConnection()) {
+            PreparedStatement prepstatement = connection.prepareStatement(sql);
+            prepstatement.setString(1, playlistName);
+            prepstatement.setInt(2, totallenght);
+            prepstatement.setInt(3, totalsongs);
+            prepstatement.addBatch();
+            prepstatement.executeBatch();
+
+            sql = "SELECT TOP(1) * FROM Playlist ORDER by Id desc";
+            prepstatement = connection.prepareStatement(sql);
+            ResultSet rs = prepstatement.executeQuery();
+            while (rs.next()) {
+                newID = rs.getInt("id");
+            }
+            prepstatement.executeBatch();
+        }
+            return new Playlist(newID, playlistName, totallenght, totalsongs);
+    }
+    public void updatePlaylist(Playlist playlistUpdate) throws SQLException {
+        try(Connection connection = databaseConnector.getConnection()) {
+            String sql = "UPDATE Playlist SET PlaylistName=?, Totallenght=?, Totalsongs=? WHERE Id = ?;";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, playlistUpdate.getPlaylistname());
+            ps.setInt(2, playlistUpdate.getTotallenght());
+            ps.setInt(3, playlistUpdate.getTotalSongs());
+            ps.setInt(4, playlistUpdate.getId());
+            if (ps.executeUpdate() != 1) {
+                throw new Exception("Could not update playlist");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deletePlaylist(Playlist playlistDelete) {
+        try (Connection connection = databaseConnector.getConnection()) {
+            String sql = "DELETE from Playlist WHERE Id = ?";
+            PreparedStatement preparedStmt = connection.prepareStatement(sql);
+            preparedStmt.setString(1, Integer.toString(playlistDelete.getId()));
+            preparedStmt.execute();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
 
     public void addSongToPlaylist(int playlistId, int songId)
     {
@@ -54,7 +102,5 @@ public class PlaylistDAO {
             System.out.println(ex);
         }
     }
-
-
 
 }
