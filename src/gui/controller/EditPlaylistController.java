67f286bc511cc369e88412a08364a624bb1bf721
kasp441 <91524039+kasp441@users.noreply.github.com>
2021-12-14 10:31:26 +0100

    Update EditPlaylistController.java

--- src/gui/controller/EditPlaylistController.java
+++ src/gui/controller/EditPlaylistController.java
@@ -1,11 +1,33 @@
 package gui.controller;
 
+import gui.model.PlaylistModel;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class EditPlaylistController {
-    public void cancelPlaylistButton(ActionEvent actionEvent) {
+
+    public TextField enterPlaylistName;
+    public Button cancelPlaylistId;
+    public Button savePlaylistId;
+    public PlaylistModel plm;
+
+    public EditPlaylistController() throws IOException {
+        plm = new PlaylistModel();
+    }
+    public void cancelPlaylistButton(ActionEvent actionEvent) { closeWindow();
     }
 
-    public void savePlaylistButton(ActionEvent actionEvent) {
+    public void savePlaylistButton(ActionEvent actionEvent)
+    {
+        //TODO
+    }
+
+    public void closeWindow() {                 //kunne laves i en utility class - evt. andet duplicate code?
+        Stage stage = (Stage) savePlaylistId.getScene().getWindow();
+        stage.close();
     }
 }
