8dd104754a2f3e0f93d1ca8155724250e0cfbdfe
kasp441 <91524039+kasp441@users.noreply.github.com>
2021-12-14 10:20:01 +0100

    created edit Playlist and song fxml and controller

---
+++ src/gui/controller/EditPlaylistController.java
@@ -0,0 +1,11 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+
+public class EditPlaylistController {
+    public void cancelPlaylistButton(ActionEvent actionEvent) {
+    }
+
+    public void savePlaylistButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/gui/controller/EditSongController.java
@@ -0,0 +1,17 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+
+public class EditSongController {
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleChoose(ActionEvent actionEvent) {
+    }
+
+    public void handleMore(ActionEvent actionEvent) {
+    }
+}
